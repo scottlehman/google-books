0566a99aec453e07d3f9f0e8a242788c8fc8128f
Scott Lehman <scottsgllehman@gmail.com>
2019-05-11 11:04:41 -0600

    updating for mern stack workflow

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import NavBar from './components/NavBar';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <Fragment>
+      <NavBar />
+      <Header />
+    </Fragment>
+
+  );
+}
+
+export default App;
